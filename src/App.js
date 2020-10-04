cc99121ee5f9378a6d7c1d31cc4e804caf75a50f
Sunny Gupta <sunny.gupta@primussoft.com>
2020-10-04 20:22:14 +0530

    tutorial updates

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import Test from './test';
 function App() {
   var myStyle = {
     fontSize: 100,
@@ -25,7 +25,10 @@ function App() {
     //   </header>
     // </div>
     <div>
-            Hello World!!!
+            <Test/>
+            <Car Color="red"/>
+            <MyForm/>
+            <Football />
             <h1>{1+1}</h1>
             {/* <h1>{i == 1 ? 'True!' : 'False'}</h1> */}
             <Header/>
@@ -46,3 +49,97 @@ class Header extends React.Component {
      );
   }
 }
+
+
+
+
+ class Car extends React.Component {
+   render() {
+     return <h2>I am a {this.props.color} Car!</h2>;
+   }
+ }
+
+//  class Football extends React.Component {
+
+//    constructor(props) {
+//       super(props)
+//       this.shoot = this.shoot.bind(this)
+//     }
+//    shoot = () => {
+//      console.log(this)
+//      /*
+//      The 'this' keyword refers to the component object
+//      */
+//    }
+//    render() {
+//      return (
+//        <button onClick={this.shoot}>Take the shot!</button>
+//      );
+//    }
+//  }
+
+class Football extends React.Component {
+   shoot = (a, b) => {
+     alert(b.type);
+     /*
+     'b' represents the React event that triggered the function,
+     in this case the 'click' event
+     */
+   }
+   render() {
+     return (
+      //  <button onClick={(ev) => this.shoot("Goal", ev)}>Take the shot!</button>
+      <button onClick={this.shoot.bind(this, "Goal")}>Take the shot!</button>
+     );
+   }
+ }
+ 
+//  class MyForm extends React.Component {
+//    constructor(props) {
+//      super(props);
+//      this.state = { username: '' };
+//    }
+//    myChangeHandler = (event) => {
+//      this.setState({username: event.target.value});
+//    }
+//    render() {
+//      return (
+//        <form>
+//        <h1>Hello {this.state.username}</h1>
+//        <p>Enter your name:</p>
+//        <input
+//          type='text'
+//          onChange={this.myChangeHandler}
+//        />
+//        </form>
+//      );
+//    }
+//  }
+
+class MyForm extends React.Component {
+   constructor(props) {
+     super(props);
+     this.state = { username: '' };
+   }
+   myChangeHandler = (event) => {
+     this.setState({username: event.target.value});
+   }
+   render() {
+     let header = '';
+     if (this.state.username) {
+       header = <h1>Hello {this.state.username}</h1>;
+     } else {
+       header = '';
+     }
+     return (
+       <form>
+       {header}
+       <p>Enter your name:</p>
+       <input
+         type='text'
+         onChange={this.myChangeHandler}
+       />
+       </form>
+     );
+   }
+ }
